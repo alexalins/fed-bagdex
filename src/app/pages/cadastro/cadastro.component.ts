c83707aac8e16f742f2ab61f98d7c085953eee00
alexalins <alexa@glinsoo.com>
2023-09-29 16:47:26 -0300

    fznd form da tela de cadastro

--- src/app/pages/cadastro/cadastro.component.ts
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
@@ -12,4 +13,24 @@ export class CadastroComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  formCadastro = new FormGroup({
+    nome: new FormControl('', [Validators.minLength(4), Validators.required]),
+    email: new FormControl('', [Validators.email, Validators.required]),
+    senha: new FormControl('', [Validators.minLength(4), Validators.required]),
+  });
+
+  cadastrar() {
+
+  }
+
+  get formPreenchido() {
+    return (
+      this.formCadastro.controls['nome'].errors?.['required'] &&
+      this.formCadastro.controls['nome'].touched ||
+      this.formCadastro.controls['email'].errors?.['required'] &&
+      this.formCadastro.controls['email'].touched ||
+      this.formCadastro.controls['senha'].errors?.['required'] &&
+      this.formCadastro.controls['senha'].touched
+    );
+  }
 }
