6aca3beed326a4184afe7def0934d1077def9ae3
alexalins <alexa@glinsoo.com>
2023-09-29 17:31:34 -0300

    criando classe de constantes

---
+++ src/app/shared/utils/constants.ts
@@ -0,0 +1,10 @@
+export class Constants {
+  //Pages
+  static readonly URL_LOGIN = '/login';
+  static readonly URL_CADASTRO = '/cadastro';
+
+  //HTTPS CODE
+  static readonly UNAUTHORIZED: number = 401;
+  static readonly CONFLICT: number = 409;
+
+}
