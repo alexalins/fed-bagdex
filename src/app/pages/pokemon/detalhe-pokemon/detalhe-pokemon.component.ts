f45f546317178ebab43676e6daaa2e9faba09f4e
alexalins <alexa@glinsoo.com>
2024-02-06 19:53:40 -0300

    criando card de detalhes de pokemon

--- src/app/pages/pokemon/detalhe-pokemon/detalhe-pokemon.component.ts
+++ src/app/pages/pokemon/detalhe-pokemon/detalhe-pokemon.component.ts
@@ -1,4 +1,10 @@
+import { ToastrService } from 'ngx-toastr';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PokemonApiResponse } from 'src/app/shared/model/response/pokemonApi';
+import { PokemonService } from 'src/app/shared/services/pokemon.service';
+import { environment } from 'src/environments/environment';
+import { DataUtil } from 'src/app/shared/utils/dataUtil';
 
 @Component({
   selector: 'app-detalhe-pokemon',
@@ -7,9 +13,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetalhePokemonComponent implements OnInit {
 
-  constructor() { }
+  pokemon: PokemonApiResponse = new PokemonApiResponse();
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private pokemonService: PokemonService,
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    let nome = this.activatedRoute.snapshot.paramMap.get('nome')!;
+    this.getDetalhePokemon(nome);
   }
 
+
+  getDetalhePokemon(nome: string = '') {
+    this.pokemonService.getPokemon(nome).subscribe(
+      (data: PokemonApiResponse) => {
+        this.pokemon = data;
+      },
+      (erro) => {
+        this.toastr.error('Erro ao buscar os dados do pokémon na PokéApi!');
+      }
+    )
+  }
+
+  get urlFoto() {
+    return `${environment.apiPokePicture}/${DataUtil.adicionaZerosAEsquerda(this.pokemon.id)}.png`;
+  }
 }
