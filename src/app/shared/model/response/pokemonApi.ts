355eb1cf4260b2c164a7e56992177f8bae77159a
alexalins <alexa@glinsoo.com>
2024-02-19 19:49:02 -0300

    ajuste na classes de pokemon

--- src/app/shared/model/response/pokemonApi.ts
+++ src/app/shared/model/response/pokemonApi.ts
@@ -1,10 +1,12 @@
+import { Stats } from "../stats";
+
 export class PokemonApiResponse {
   id: number;
   name: string;
   url: string;
   height: number;
   types: string [];
-  stats: string [];
+  stats: Stats [];
 
   constructor() {
     this.id = 0;
@@ -13,5 +15,13 @@ export class PokemonApiResponse {
     this.height = 0;
     this.types = [];
     this.stats = [];
+  }
+
+  setTypes(types: any[]) {
+    types.forEach(type => this.types.push(type.type.name))
+  }
+
+  setStats(stats: any[]) {
+    stats.forEach(stat => this.stats.push(new Stats(stat?.base_stat, stat?.stat?.name)));
   }
 }
---
+++ src/app/shared/model/stats.ts
@@ -0,0 +1,9 @@
+export class Stats {
+  base: number;
+  nome: string;
+
+  constructor(base: number, nome: string) {
+    this.base = base;
+    this.nome = nome;
+  }
+}
